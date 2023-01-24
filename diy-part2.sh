c0ef6cc6e3b6c66756b434b7d5f4e7005a68728f
PeDitX <pedram.ale@me.com>
2023-01-24 06:57:56 +0330

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,12 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# wifI
+sed -i 's/ssid=OpenWrt/ssid=MIWIFI_PeDitXrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# set wifi passkey to 123456789
+sed -i 's/encryption=none/encryption=sae-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# set password
+sed -i '/set wireless.default_radio${devidx}.encryption=sae-mixed/a\set wireless.default_radio${devidx}.key=123456789' package/kernel/mac80211/files/lib/wifi/mac80211.sh
